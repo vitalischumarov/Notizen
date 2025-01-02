7688bf1f9f79bad5ec3cefc1ad6f34d7d5172272
Vitali Schumarov <v.schumarov@icloud.com>
2025-01-02 18:30:16 +0100

    buttons connected

---
+++ script.js
@@ -0,0 +1,11 @@
+function create_new_note() {
+  console.log(`new note created`);
+}
+
+function save_note() {
+  console.log(`save note`);
+}
+
+function delete_note() {
+  console.log(`delete note`);
+}
