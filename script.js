e7fd7994524cca6eccff46dadfc9ae2a702dc6a4
Vitali Schumarov <v.schumarov@icloud.com>
2025-01-03 14:45:20 +0100

    notes are sorted

--- script.js
+++ script.js
@@ -3,29 +3,41 @@ const all_notes = [
     id: 1,
     title: "Title A",
     note_message: "message A",
-    timestamp: "01.03.2025",
+    timestamp: "2033-01-03",
   },
   {
     id: 2,
     title: "Title B",
     note_message: "message B",
-    timestamp: "09.09.2023",
+    timestamp: "2021-01-03",
   },
   {
     id: 3,
     title: "Title C",
     note_message: "message C",
-    timestamp: "03.10.2019",
+    timestamp: "2024-01-03",
   },
   {
     id: 4,
     title: "Title D",
     note_message: "message D",
-    timestamp: "11.09.2020",
+    timestamp: "2022-01-03",
+  },
+  {
+    id: 4,
+    title: "Title D",
+    note_message: "message D",
+    timestamp: "2028-01-03",
   },
 ];
 
 window.addEventListener("load", (event) => {
+  all_notes.sort((a, b) => {
+    const dateA = new Date(a.timestamp);
+    const dateB = new Date(b.timestamp);
+    return dateB - dateA;
+  });
+  console.log(all_notes);
   for (let i = 0; i < all_notes.length; i++) {
     create_note_UI(
       all_notes[i].title,
